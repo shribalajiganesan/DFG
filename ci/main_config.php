7f694200ba6c4cf681cf2c5bbc33ccb0e947c687
prasaath <prasaathzcodia@gmail.com>
2017-08-01 13:28:24 +0530

    Config change

---
+++ ci/main_config.php
@@ -0,0 +1,9 @@
+<?php
+
+define("baseURL", 'http://' . $_SERVER['HTTP_HOST'] . "/_dfg"); //for local
+//define("baseURL", 'http://' . $_SERVER['HTTP_HOST']) . '/'; //for live
+
+define("host", 'e4buzz.com');
+define("database", 'efoubuzz_dfg_oc');
+define("username", 'efoubuzz_dfg_oc');
+define("pass", 'Shri@123');
